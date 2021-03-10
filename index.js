cb7ff236cb75d944be4a9576195e03ac1218fb3d
jerrysjoseph <jerin.sebastian153@gmail.com>
2021-03-10 08:55:49 +0000

    new html update

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express=require('express');
+const io=require('socket.io-client');
+const app=express();
+
+const socket=io.connect('http://192.168.1.26:3001',{reconnection:true});
+
+app.use(express.static(__dirname+'/public'));
+
+const PORT=5000;
+socket.on('connect',()=>{
+    console.log('connected to Server')
+    socket.emit('hub-handshake',"Hi from hub1",(response)=>{
+        console.log(response);
+    })
+    
+})
+socket.on('command',(msg)=>{
+    console.log("Command recieved: "+msg);
+})
+
+app.listen(PORT,()=>console.log("Hub server is listening on port "+PORT))
