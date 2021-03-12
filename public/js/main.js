434fe9d9643be09e1a699d333e95098e7e4f4917
jerrysjoseph <jerin.sebastian153@gmail.com>
2021-03-12 14:22:33 +0530

    ui changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,48 @@
 
 const REGISTER_ROUTE='/api/v1/hub/register';
+const LOGIN_ROUTE='/api/v1/hub/login';
+
+
+
+//registering events
 
 $( "#register_button" ).click(function() {
+        register();
+  });
+
+$( "#registermodal" ).keypress(function(e) {
+    
+    if(e.which===13)
+        register();
+});
+$( "#loginmodal" ).keypress(function(e) {
+    
+    if(e.which===13)
+        login();
+});
+  $("#login_button").click(function() {
+        login();
+  });
+
+
+// methods
+
+function register()
+{
+    var proceed=true;
 
     //extracting the data from DOM elements 
-    const ip=$('#server_ip').val();
-    const name=$('#hub_name').val();
-    const pass=$('#hub_pass').val();
+    const ip=$('#server_ip_register').val();
+    const name=$('#hub_name_register').val();
+    const pass=$('#hub_pass_register').val();
     const data={
         name:name,
         password:pass
     };
-
+    if(!ip || ip.length<1)
+    {
+        $('#server_ip_register').error()
+    }
     //fetching the API response
     const url=ip+REGISTER_ROUTE;
 
@@ -26,19 +57,66 @@ $( "#register_button" ).click(function() {
     .then(jsondata=>{
         if(jsondata.success)
             {
-                showAlert(`Your hub has been registered successfully. Close this dialog and Login with Hub name : ${jsondata.msg.name} and the your password`,'alert-success');
+                showAlertRegister(`Your hub has been registered successfully. Close this dialog and Login with Hub name : ${jsondata.msg.name} and the your password`,'alert-success');
             }
             else
-            showAlert(`<strong>Error</strong>: ${jsondata.msg}`,'alert-danger')
+            showAlertRegister(`<strong>Error</strong>: ${jsondata.msg}`,'alert-danger')
     })
     .catch(err=>console.log(err));
+}
 
-  });
+function login() {
+    //extracting the data from DOM elements 
+    const ip=$('#server_ip_login').val();
+    const name=$('#hub_name_login').val();
+    const pass=$('#hub_pass_login').val();
+    const data={
+        name:name,
+        password:pass
+    };
+
+    //fetching the API response
+    const url=ip+LOGIN_ROUTE;
+
+    fetch(url,{
+        method: "POST",
+        headers:{
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          },
+        body:JSON.stringify(data)
+    })
+    .then(response=>response.json())
+    .then(jsondata=>{
+
+        if(jsondata.success)
+            {
+                
+                
+                window.location.href='/dashboard.html'
+            }
+            else
+            showAlertLogin(`<strong>Error</strong>: ${jsondata.msg}`,
+                'alert-danger');
+    })
+    .catch(err=>console.log(err));
+}
+
+function showAlertRegister( message, alerttype ) {
+
+    $('#alert_placeholder_register').html(' <div class="alert '+alerttype+'" id="alertdiv" role="alert">'+message+'</div>')
+
+    // close it in 3 secs
+    setTimeout( function() {
+            $("#alertdiv").remove();
+        }, 10000);
+
+}
 
 
-function showAlert( message, alerttype ) {
+function showAlertLogin( message, alerttype ) {
 
-    $('#alert_placeholder').html(' <div class="alert '+alerttype+'" id="alertdiv" role="alert">'+message+'</div>')
+    $('#alert_placeholder_login').html(' <div class="alert '+alerttype+'" id="alertdiv" role="alert">'+message+'</div>')
 
     // close it in 3 secs
     setTimeout( function() {
